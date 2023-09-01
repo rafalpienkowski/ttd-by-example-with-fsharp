1814c3f05d1385f2724ba4c8ad2445fe03427066
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2023-09-01 07:45:55 +0200

    Move equality comparison to Money class.

--- Money/Dollar.cs
+++ Money/Dollar.cs
@@ -1,67 +1,63 @@
 namespace Money;
 
-public abstract class Money
+public abstract class Money: IEquatable<Money>, IEqualityComparer<Money>
 {
-    protected int Amount;
-}
+    protected readonly int Amount;
 
-public class Dollar : Money, IEquatable<Dollar>
-{
-    public Dollar(int amount)
+    protected Money(int amount)
     {
         Amount = amount;
+    }
+
+    public bool Equals(Money? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Amount == other.Amount;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == GetType() && Equals((Money)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Amount;
+    }
+
+    public bool Equals(Money? x, Money? y)
+    {
+        if (ReferenceEquals(x, y)) return true;
+        if (ReferenceEquals(x, null)) return false;
+        if (ReferenceEquals(y, null)) return false;
+        if (x.GetType() != y.GetType()) return false;
+        return x.Amount == y.Amount;
+    }
+
+    public int GetHashCode(Money obj)
+    {
+        return obj.Amount;
+    }
+}
+
+public class Dollar : Money
+{
+    public Dollar(int amount) : base(amount)
+    {
     }
 
     public Dollar Times(int multiplier) => new(Amount * multiplier);
-
-    public bool Equals(Dollar? other)
-    {
-        if (ReferenceEquals(null, other)) return false;
-        if (ReferenceEquals(this, other)) return true;
-        return Amount == other.Amount;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (ReferenceEquals(null, obj)) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
-        return Equals((Dollar)obj);
-    }
-
-    public override int GetHashCode()
-    {
-        return Amount;
-    }
 }
 
-public class Franc : Money, IEquatable<Franc>
+public class Franc : Money
 {
 
-    public Franc(int amount)
+    public Franc(int amount) : base(amount)
     {
-        Amount = amount;
     }
     
     public Franc Times(int multiplier) => new(Amount * multiplier);
-
-    public bool Equals(Franc? other)
-    {
-        if (ReferenceEquals(null, other)) return false;
-        if (ReferenceEquals(this, other)) return true;
-        return Amount == other.Amount;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (ReferenceEquals(null, obj)) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
-        return Equals((Franc)obj);
-    }
-
-    public override int GetHashCode()
-    {
-        return Amount;
-    }
 }
