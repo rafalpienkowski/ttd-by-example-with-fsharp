4f8e792a2c9d8ea45b09744d434afdccc7093dab
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2023-09-03 14:04:20 +0200

    Refactor Sum to record

--- Money/Sum.cs
+++ Money/Sum.cs
@@ -1,16 +1,7 @@
 namespace Money;
 
-public class Sum : IExpression
+public record Sum(IExpression Augend, IExpression Added) : IExpression
 {
-    public readonly IExpression Added;
-    public readonly IExpression Augend;
-
-    public Sum(IExpression augend, IExpression added)
-    {
-        Augend = augend;
-        Added = added;
-    }
-
     public Money Reduce(Bank bank, string to)
     {
         var amount = Augend.Reduce(bank, to).Amount + Added.Reduce(bank, to).Amount;
